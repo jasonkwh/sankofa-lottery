2fb688d5bd5f7dcd97a7686eb51e619966ddbc53
Jason Wong <jasonkwh@gmail.com>
2016-11-30 00:40:59 +1100

    Fix keyboard and ui bug

--- sankofa-lottery/FirstViewController.swift
+++ sankofa-lottery/FirstViewController.swift
@@ -26,6 +26,16 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
